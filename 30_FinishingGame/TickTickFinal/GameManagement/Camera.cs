0a89fa63a96609c1f5ecba6c1cdca9cb32e06812
John <HylianNanobyte@Gmail.com>
2016-11-04 15:28:48 +0100

    also Camera something

---
+++ 30_FinishingGame/TickTickFinal/GameManagement/Camera.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class Camera
+{
+    Vector2 position;
+    int levelWidth;
+    int screenWidth;
+
+    public Camera()
+    {
+        position = Vector2.Zero;
+        levelWidth = 2000;
+        screenWidth = 1000;
+    }
+
+    public void StartPosition(int x, int y)
+    {
+        position.X = x;
+        position.Y = y;
+    }
+
+    public void SetPosition(int x, int y)
+    {
+        if(x < screenWidth / 2)
+        {
+            position.X = screenWidth / 2;
+        }
+        else if(x > levelWidth - screenWidth / 2)
+        {
+            position.X = levelWidth - screenWidth / 2;
+        }
+        else
+        {
+            position.X = x;
+        }
+    }
+}
+
